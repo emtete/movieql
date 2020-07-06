7cfb8dda2d27e698528311c4bc1ffa5f1fb90d63
최종호 <victor_77@naver.com>
2020-07-06 11:26:31 +0900

    change fetch -> axios

--- graphql/db.js
+++ graphql/db.js
@@ -1,8 +1,16 @@
-import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+import axios from "axios";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
-export const getMovies = (limit, lating) => {
-  return fetch(`${API_URL}`)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
+export const getMovies = async (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  const movies = (await axios(REQUEST_URL)).data.data.movies;
+
+  return movies;
 };
