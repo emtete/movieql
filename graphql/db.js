1e41136202c35c6fbb9bfcb48a7afd8591defc3a
최종호 <victor_77@naver.com>
2020-07-06 11:55:09 +0900

    add getMovie method

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,23 @@
 import axios from "axios";
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
+const list_movies = "https://yts.mx/api/v2/list_movies.json?";
+const detail_movie = "https://yts.mx/api/v2/movie_details.json?";
 
 export const getMovies = async (limit, rating) => {
-  let REQUEST_URL = API_URL;
+  let REQUEST_URL = list_movies;
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`;
   }
   if (rating > 0) {
     REQUEST_URL += `&minimum_rating=${rating}`;
   }
+  // return movies
+  return (await axios(REQUEST_URL)).data.data.movies;
+};
 
-  const movies = (await axios(REQUEST_URL)).data.data.movies;
-
-  return movies;
+export const getMovie = async (id) => {
+  if (!id) return;
+  let REQUEST_URL = detail_movie;
+  const returnData = await axios(REQUEST_URL, { params: { movie_id: id } });
+  // return movies
+  return returnData.data.data.movie;
 };
